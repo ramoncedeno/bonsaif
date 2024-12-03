9f898f72967e85d10ab9fafcc0e069748fc1eb7f
Ramon Cedeno <rcedeno@uinenlinea.mx>
2024-12-02 20:56:17 -0600

    SmsTransactionController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\SmsTransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/send-sms/{phone}/{message}', [SmsTransactionController::class, 'sendSMS'])->name('sms.send');
+Route::post('/send-sms/{phone}/{message}', [SmsTransactionController::class, 'sendSMS'])->name('sms.send');
